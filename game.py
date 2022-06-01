07a30c02a793a64b78f228ba608bc2fb09a4af39
Rodney McGee <rodneymcgee73@gmail.com>
2022-05-31 21:18:07 -0500

    Created play_game, and display_greeting functions.

---
+++ game.py
@@ -0,0 +1,14 @@
+
+class Game:
+    
+    def __init__(self):
+        print()
+
+    def play_game(self):
+       self.display_greeting()
+       self.get_player_count()
+    
+    def display_greeting(self):
+        print("Welcome player and/or players to Rock, Paper, Scissors, Lizard, Spock!")
+    
+        
